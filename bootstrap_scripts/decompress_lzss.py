9472caf02c7d9f77a1b1d0d8cda8020a95b7e7c3
DirectorX <DirectorX@users.noreply.github.com>
2020-03-01 19:17:26 +0200

    decompress_lzss: convert to python3 closes #12

--- bootstrap_scripts/decompress_lzss.py
+++ bootstrap_scripts/decompress_lzss.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python2
 """
 /**************************************************************
  LZSS.C -- A Data Compression Program
@@ -28,16 +27,17 @@ F = 18
 THRESHOLD = 2
 NIL = N
 
-def decompress_lzss(str):
-    if str[:8] !="complzss":
-        print "decompress_lzss: complzss magic missing"
+
+def decompress_lzss(data):
+    if data[:8] != b"complzss":
+        print('decompress_lzss: complzss magic missing')
         return
-    decompsize = struct.unpack(">L", str[12:16])[0]
-    text_buf = array("B", " "*(N + F - 1))
-    src = array("B", str[0x180:])
-    #srclen = len(src)
-    srclen = struct.unpack(">L", str[16:20])[0]
-    dst = array("B", " "*decompsize)
+    decompsize = struct.unpack(">L", data[12:16])[0]
+    text_buf = array("B", b" " * (N + F - 1))
+    src = array("B", data[0x180:])
+    # srclen = len(src)
+    srclen = struct.unpack(">L", data[16:20])[0]
+    dst = array("B", b" " * decompsize)
     r = N - F
     srcidx, dstidx, flags, c = 0, 0, 0, 0
 
@@ -46,32 +46,42 @@ def decompress_lzss(str):
         if ((flags & 0x100) == 0):
             if (srcidx >= srclen):
                 break
-            c = src[srcidx]; srcidx += 1
-            flags = c | 0xFF00;
+            c = src[srcidx]
+            srcidx += 1
+            flags = c | 0xFF00
 
         if (flags & 1):
             if (srcidx >= srclen):
                 break
-            c = src[srcidx]; srcidx += 1
-            dst[dstidx] = c; dstidx += 1
-            text_buf[r] = c; r += 1
-            r &= (N - 1);
+            c = src[srcidx]
+            srcidx += 1
+            dst[dstidx] = c
+            dstidx += 1
+            text_buf[r] = c
+            r += 1
+            r &= (N - 1)
         else:
             if (srcidx >= srclen):
                 break
-            i = src[srcidx]; srcidx += 1
+            i = src[srcidx]
+            srcidx += 1
             if (srcidx >= srclen):
                 break
-            j = src[srcidx]; srcidx += 1
+            j = src[srcidx]
+            srcidx += 1
             i |= ((j & 0xF0) << 4)
-            j  =  (j & 0x0F) + THRESHOLD
-            for k in xrange(j+1):
+            j = (j & 0x0F) + THRESHOLD
+            for k in range(j + 1):
                 c = text_buf[(i + k) & (N - 1)]
-                dst[dstidx] = c; dstidx += 1
-                text_buf[r] = c; r += 1
+                dst[dstidx] = c
+                dstidx += 1
+                text_buf[r] = c
+                r += 1
                 r &= (N - 1)
-    return dst.tostring()
+    return dst.tobytes()
+
 
 if __name__ == "__main__":
     data = decompress_lzss(open(sys.argv[1], "rb").read())
-    open(sys.argv[2], "wb").write(data)
+    if data:
+        open(sys.argv[2], "wb").write(data)
